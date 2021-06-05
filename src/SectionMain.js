b83c371f1c978ca863c0f24e03915e7f758ee0a9
Elliott Watson <elliottwatson0@gmail.com>
2021-06-05 12:23:57 -0400

    more comments

--- src/SectionMain.js
+++ src/SectionMain.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './SectionMain.css';
-
+// Building the section main structure using class components to use in app.js w classname 
 class SectionMain extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //allows import of index.css file 
 import './index.css';
+//allowing use of app component  
 import App from './App';
+//allowing data to update date offline
 import registerServiceWorker from './registerServiceWorker';
 
-
+//controls the contents of App
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
